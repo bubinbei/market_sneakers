4847fa408607fdcb9a5de8c48e95a601abb7f442
Roman Borodulin <bubinbei@gmail.com>
2021-10-03 17:32:06 +0400

    plus - add - chek - car

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './Card.module.scss'
 
 function Card({data}) {
+  const [add, setAdd] = useState(false)
+  const onClickAdd = () => setAdd(!add)
   const {num, prise, name} = data
-  console.log('====================================');
-  console.log(data);
-  console.log('====================================');
+  
+
   return (
-    <div className="card">
-      <div className="favorite">
+    <div className={styles.card}>
+      <div className={styles.favorite}>
       <img src="/img/heart-off.svg" alt="heart"/>
       </div>
           <img width={133} height={112} src = {num} alt="Sneakers" />
           <h5>{name}</h5>
+          
           <div className="d-flex justify-between align-center">
             <div className="d-flex flex-column">
               <span>Цена:</span>
               <b>{prise*1000} руб.</b>
             </div>
-            <button className="button">
-              <img width={11} height={11} src="/img/plus.svg" alt="Plus"/>
-            </button>
-          </div>
+              <img onClick={onClickAdd} src={add?"/img/chek.svg":"/img/plus2.svg"} alt="Plus"/>
+            </div>
         </div>
   );
 }
